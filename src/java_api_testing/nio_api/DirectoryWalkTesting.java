bf841d82ff0aa63d342d54c0c2f590969ef1c928
Amvrosov <amvrosovalex@gmail.com>
2017-03-25 22:09:50 +0800

    Обновление классов и примеров

--- src/java_api_testing/nio_api/DirectoryWalkTesting.java
+++ src/java_api_testing/nio_api/DirectoryWalkTesting.java
@@ -1,11 +1,24 @@
 package java_api_testing.nio_api;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.nio.file.DirectoryStream;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.util.List;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 import javax.swing.BorderFactory;
 import javax.swing.DefaultListModel;
@@ -15,8 +28,11 @@ import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
 import javax.swing.event.DocumentEvent;
@@ -34,8 +50,18 @@ public class DirectoryWalkTesting {
 	// Для получения объектов Path используется объект java.nio.file.FileSystem (получаемый из фабрики java.nio.file.FileSystems)
 	Path mCurrentPath = null;
 	
+	ExecutorService mExecutorService = Executors.newSingleThreadExecutor();
+	Future<?> mWatchingFuture = null;
+	
+	JTextArea mWatchingLogArea = new JTextArea();
+	
+	{
+		mWatchingLogArea.setEditable(false);
+		mWatchingLogArea.setFont( new Font(Font.MONOSPACED, 0, 13) );
+	}
+	
 	public DirectoryWalkTesting () {
-		JFrame myFrame = new JFrame ( "Text Area Template" );
+		JFrame myFrame = new JFrame ( "Directory Walk and Update Testing" );
 		myFrame.setSize(700, 800);
 		myFrame.setLocationRelativeTo(null);
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -86,6 +112,20 @@ public class DirectoryWalkTesting {
 			}
 		});
 		
+		JScrollPane area_scroll = new JScrollPane(mWatchingLogArea);
+		mWatchingLogArea.setPreferredSize(
+				new Dimension(mWatchingLogArea.getPreferredSize().width, 200));
+		mWatchingLogArea.setBorder( BorderFactory.createTitledBorder("File system update monitor:") );
+		JPopupMenu area_menu = new JPopupMenu();
+		area_menu.add(new JMenuItem("Clear...")).addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				mWatchingLogArea.setText("");
+			}
+		});
+		mWatchingLogArea.setComponentPopupMenu(area_menu);
+		main_panel.add(area_scroll, BorderLayout.SOUTH);
+		
 		JFileChooser directory_chooser = new JFileChooser();
 		directory_chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 		choose_directory_btn.addActionListener(new ActionListener() {
@@ -107,6 +147,7 @@ public class DirectoryWalkTesting {
 		if (mCurrentPath == null) {
 			return "Choose directory to list its files...";
 		}
+		
 		// При помощи статического метода java.nio.file.Files.newDirectoryStream мы можем перебрать содержимое заданной дирректории:
 		try ( DirectoryStream<Path> found_paths =
 				( (file_filter.isEmpty()) 
@@ -114,15 +155,70 @@ public class DirectoryWalkTesting {
 				: (Files.newDirectoryStream(mCurrentPath, file_filter)) ) // Перебор только тех путей, которые соответствуют заданному шаблону
 			) 
 		{
+			// Прерываем выполнение предыдущей задачи по отслеживанию изменений:
+			if ( mWatchingFuture != null ) {
+				mWatchingFuture.cancel(true);
+				try {
+					mWatchingFuture.get(1000, TimeUnit.MILLISECONDS);
+				} catch ( CancellationException e ) {}
+			}
+			
 			for ( Path path_elem : found_paths ) {
 				if ( !Files.isDirectory(path_elem) ) { // Помещаем имена найденных файлов в список:
 					list_model.addElement(path_elem.getFileName().toString());
 				}
 			}
+			
+			// Запускаем задачу по отслеживанию изменений в текущей выбранной папке mCurrentPath:
+			mWatchingFuture = mExecutorService.submit( new Runnable() {
+				@Override public void run() {
+					try {
+						// Создаем объект WatchService для текущей файловой системы:
+						WatchService watchService = FileSystems.getDefault().newWatchService();
+						
+						// Регистрируем сервис в объекте Watchable (им служит сам объект Path):
+						mCurrentPath.register(watchService, 		 // Подписываемся на события:
+								StandardWatchEventKinds.ENTRY_CREATE,// Создания файлов
+								StandardWatchEventKinds.ENTRY_DELETE,// Удаления файлов
+								StandardWatchEventKinds.ENTRY_MODIFY // Изменения файлов
+								);
+						
+						while ( !Thread.interrupted() ) {
+							// Метод WatchService.take() блокирует выполнение потока до тех пор,
+							// пока не произойдет событие в файловой системе.
+							// Информация о событии возвращается в объекте WatchKey:
+							WatchKey changeKey = watchService.take();
+							
+							// Получаем список произошедших событий из объекта WatchKey:
+							List<WatchEvent<?>> watchEvents = changeKey.pollEvents();
+							
+							for ( WatchEvent<?> watchEvent : watchEvents ) {
+								// Объектом события WatchEvent служит объект Path:
+								Path eventPath = (Path) watchEvent.context();
+								
+								// Вид произошедшего события мы можем узнать из метода WatchEvent.kind():
+								printToWatchingArea ( watchEvent.kind().name() + ": " + eventPath.toAbsolutePath() );
+							}
+							
+							// Важно не забыть вызов метода reset() после завершения работы с WatchKey:
+							changeKey.reset();
+						}
+					} catch ( InterruptedException e ) {
+					} catch ( Exception e ) {
+						printToWatchingArea ( e.getClass().getName() + " : " + e.getMessage() );
+					}
+				}
+			});
+			
 			return mCurrentPath.toString() + ":";
 		} catch ( Exception e ) {
 			return e.getClass().getName() + " : " + e.getMessage();
 		}
+	}
+	
+	private void printToWatchingArea ( String text ) {
+		mWatchingLogArea.append( String.format("%1$tT.%1$tL %2$s\n", new java.util.Date(), text) );
+		mWatchingLogArea.repaint();
 	}
 
 	public static void main(String[] args) {
